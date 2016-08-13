3dddf967df6896a0f7f6763897c32d2e84ac4944
Antonio-M. Corbi Bellot <antonio.corbi@ua.es>
2016-08-13 20:37:42 +0200

    Completed the cipher!

--- source/app.d
+++ source/app.d
@@ -5,28 +5,7 @@ void main()
 {
 	writeln("Edit source/app.d to start your project.");
 
-    // writeln ("Shift z by 3: ", shift (3, 'z'));
-    // writeln ("Shift c by -3: ", shift (-3, 'c'));
-    // writeln ("Shift ' ' by 3: ", shift (3, ' '));
-
-    // writeln ("'z': ", cast(int)'z');
-    // writeln ("l2i 'z': ", let2int('z'));
-    // writeln ("'c': ", cast(int)'c');
-    // writeln ("l2i 'c': ", let2int('c'));
-
-    // foreach (s ; -3..0) {
-    //   writefln ("Shift c by %s: %c", s, shift (s, 'c'));
-    // }
-
-    // foreach (c ; 0..26) {
-    //    writefln ("letters[%d] = %c", c, letters[c]);
-    // }
-
-    // writeln ("-1 % 26 = ", -1 % 26);
-
-    foreach (k ; 0..5) {
-      writefln ("hola (%d): %s", k, encode(k, "hola"));
-    }
-
-    writeln ("Decode jqnc = ", decode (2, "jqnc"));
+    // writeln ("programming in dlang is very fun! => ",
+    //          encode(7, "programming in dlang is very fun!"));
+    writeln ("wyvnyhttpun pu kshun pz clyf mbu! = ", crack ("wyvnyhttpun pu kshun pz clyf mbu!"));
 }
--- source/caesar.d
+++ source/caesar.d
@@ -2,19 +2,16 @@
 // The Caesar Cipher based on a Haskell implementation from "Learning Haskell".
 // (c) Antonio Corbi, GPL3 code
 
-// static this () {
-//   import std.stdio;
-  
-//   writeln ("Module constructor.");
-//   foreach (c ; 0..26) {
-//     writefln ("%c: %d", cast(char)c, c);
-//     letters[c] = cast(char)(c +'a');
-//   }
+/////////////
+// IMPORTS //
+/////////////
+import std.stdio, std.conv;
+import std.algorithm, std.algorithm.searching, std.range;
+import std.ascii, std.string : countchars;
 
-// }
-
-// char[26] letters;
-
+//////////
+// CODE //
+//////////
 int let2int (char c) {
   return cast(int) (c - 'a');
 }
@@ -33,28 +30,75 @@ char shift (int n, char c) {
 }
 
 string encode (int n, string s) {
-  import std.algorithm;
   string r;
 
   s.each!(c => r ~= shift(n, c));
   return r;
 }
 
-string decode (int n, string s) { return encode (-n, s); }
+alias decode = (int n, string s) { return encode (-n, s); };
 
 float percent (int n, int m) {
-  return (n/cast(float)m) * 100;
+  return (n / cast(float) m) * 100;
 }
 
-int count (char c, string s) {
-  import std.string : countchars;
-  import std.conv : to;
+int[] positions (float x, float[] fl) {
+  int[] il;
+  
+  auto r = zip (fl, iota(fl.length)).filter!(t => t[0]==x).map!(t => t[1]);
 
+  r.each!(a => il ~= cast(int)a);
+  
+  return il;
+}
+
+int lowers (string s) {
+  return cast(int) count!(a => a.isLower)(s);
+}
+
+int cccount (char c, string s) {
   return cast(int) countchars(s, to!string(c));
 }
 
 float[] freqs (string s) {
   float[] f;
+  auto allChars = "abcdefghijklmnopqrstuvwxyz";
+  auto n = lowers(s);
 
+  auto r = allChars.map!(a => percent (cccount(to!char(a), s), n) );
+  r.each!(a => f ~= a);
+  
   return f;
+}
+
+////////////////////////////
+// Cracking the cipher... //
+////////////////////////////
+
+float chisqr (float[] os, float[] es) {
+  return zip(os, es).map!(t => ((t[0]-t[1])^^2)/t[1]).sum;
+}
+
+float[] rotate (int n, float[] fl) {
+  float[] f;
+  
+  auto r = fl.drop(n) ~ fl.take(n);
+  r.each!(a => f ~= a);
+  
+  return f;
+}
+
+string crack (string s) {
+  // English letters frequency
+  float[] table = [8.2, 1.5, 2.8, 4.3, 12.7, 2.2, 2.0, 6.1, 7.0, 0.2, 0.8, 4.0, 2.4,
+                   6.7, 7.5, 1.9, 0.1, 6.0,  6.3, 9.1, 2.8, 1.0, 2.4, 0.2, 2.0, 0.1];
+  float[] table2 = s.freqs;
+
+  auto chitabr = iota(26).map!(n => rotate(n, table2).chisqr(table));
+  float[] chitab;
+  chitabr.each!(a => chitab ~= a);
+  auto minval = reduce!(min)(chitab);
+  auto factor = positions (minval, chitab)[0];
+  
+  return decode (factor, s);
 }
