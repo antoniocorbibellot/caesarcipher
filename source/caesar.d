fc42421b64e955544efa3b69e5c1c4f101b118fb
Antonio-M. Corbi Bellot <antonio.corbi@ua.es>
2016-08-15 20:36:32 +0200

    Formatting changes.

--- source/caesar.d
+++ source/caesar.d
@@ -7,41 +7,42 @@ import std.stdio, std.conv;
 import std.algorithm, std.algorithm.searching, std.range;
 import std.ascii, std.string : countchars;
 
-int let2int(char c) {
+int let2int(char c) pure @nogc {
   return (c - 'a').to!int;
 }
-char int2let(int n) {
+char int2let(int n) pure {
   return (n >= 0 ? 'a' + n : 'z' + n + 1).to!char;
 }
-char shift(char c, int n) {
+char shift(char c, int n) pure {
   return c.isLower ? ((c.let2int + n) % 26).int2let : c;
 }
-auto encode(int n, char[] s) {
+auto encode(int n, char[] s) pure {
   return s.map!(c => c.to!char.shift(n));
 }
-alias decode = (int n, char[] s) { return encode(-n, s); };
-auto positions(float x, float[] fl) {
+alias decode = (int n, char[] s) pure { return encode(-n, s); };
+auto positions(float x, float[] fl) pure {
   return fl.zip(fl.length.iota).filter!(t => t[0]==x).map!(t => t[1]);
 }
-float chisqr(float[] os, float[] es) {
+float chisqr(float[] os, float[] es) pure @nogc {
   return os.zip(es).map!(t => ((t[0]-t[1])^^2)/t[1]).sum;
 }
-float[] rotate(int n, float[] fl) {
+float[] rotate(int n, float[] fl) pure {
   return (fl.drop(n) ~ fl.take(n)).array;
 }
 
-string crack(string s) {
-  auto freqs(char[] s) {
-    alias cccount = (char c, char[] s) {
+string crack(string s) pure {
+  auto freqs(char[] s) pure {
+    alias cccount = (char c, char[] s) pure {
       return countchars(s, to!string(c)).to!int;
     };
-    alias percent = (int n, int m) { return n / m.to!float * 100; };
-    alias lowers = (char[] s) { return count!isLower(s).to!int; };
+    alias percent = (int n, int m) pure @nogc { return n / m.to!float * 100; };
+    alias lowers = (char[] s) pure { return count!isLower(s).to!int; };
 	
     enum allChars = "abcdefghijklmnopqrstuvwxyz".to!(char[]);
 
-    return allChars.map!(a =>
-      percent(cccount(a.to!char, s), lowers(s)) ).array;
+    return allChars.
+      map!(a => percent(cccount(a.to!char, s), lowers(s))).
+      array;
   }
   // ASCII letters frequency from 'a'..'z'
   float[] table = [
@@ -49,8 +50,9 @@ string crack(string s) {
     6.7, 7.5, 1.9, 0.1, 6.0,  6.3, 9.1, 2.8, 1.0, 2.4, 0.2, 2.0, 0.1];
   auto table2 = freqs(s.to!(char[]));
 
-  auto chitab = table.length.to!uint.iota.map!(
-    n => n.rotate(table2).chisqr(table)).array;
+  auto chitab = table.length.to!uint.iota.
+    map!(n => n.rotate(table2).chisqr(table)).
+    array;
   auto minval = chitab.reduce!min;
   auto factor = positions(minval, chitab).front.to!int;
 
