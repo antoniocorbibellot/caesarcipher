b37ca24f33177e887c99e5ec58df7b2de3d35963
Antonio-M. Corbi Bellot <antonio.corbi@ua.es>
2017-04-10 19:03:44 +0200

    Included another example.

--- source/app.d
+++ source/app.d
@@ -1,4 +1,4 @@
-import std.stdio;
+import std.stdio, std.conv;
 import caesar;
 
 void main()
@@ -7,5 +7,8 @@ void main()
 
     // writeln ("programming in dlang is very fun! => ",
     //          encode(7, "programming in dlang is very fun!"));
+    // writeln ("alumnos de programacion2 ingenieria robotica => ",
+    //          encode(17, "alumnos de programacion2 ingenieria robotica".to!(char[])));
     writeln ("wyvnyhttpun pu kshun pz clyf mbu! = ", crack ("wyvnyhttpun pu kshun pz clyf mbu!"));
+    writeln ("rcldefj uv gifxirdrtzfe2 zexvezvizr ifsfkztr = ", crack("rcldefj uv gifxirdrtzfe2 zexvezvizr ifsfkztr"));
 }
